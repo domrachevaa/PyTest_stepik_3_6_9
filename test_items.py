008d670cf8175381c3479037114de8ace737111f
domrachevaa <87689881+domrachevaa@users.noreply.github.com>
2021-10-01 18:38:08 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_btn_add_to_basket(browser):
     browser.get(link)
     btn = browser.find_elements_by_css_selector("button[class*='btn-add-to-basket']")
     # time.sleep(2)
