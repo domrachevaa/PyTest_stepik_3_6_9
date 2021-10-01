fad6147f6cfa53bee93e19ed4667c0028c5afb2c
domrachevaa <87689881+domrachevaa@users.noreply.github.com>
2021-10-01 18:48:35 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_btn_add_to_basket(browser):
     browser.get(link)
+    browser.implicitly_wait(15)
     btn = browser.find_elements_by_css_selector("button[class*='btn-add-to-basket']")
     # time.sleep(2)
     assert len(btn) > 0, "Кнопка добавления в корзину - отсутствует"
